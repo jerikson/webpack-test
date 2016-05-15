3258627a473a9b6a8d14f1627979aaa402ba5b0c
jerikson <jimmyerikson@gmail.com>
2016-05-16 00:17:25 +0200

    Added npm build weback scrip to package.json

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 const PATHS = {
-  app: path.join(__dirrname, 'app'),
-  build: path.join(__dirrname, 'build')
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
 };
 
 module.exports = {
@@ -11,6 +11,6 @@ module.exports = {
   },
   output: {
     path: PATHS.build,
-    filename: "bundle.js"
+    filename: 'bundle.js'
   }
 };
